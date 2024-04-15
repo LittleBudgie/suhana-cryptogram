7b04bb011729d3f6c4360f0f920f350c139f671c
Suhana Shrivastava <69771816+LittleBudgie@users.noreply.github.com>
2024-04-14 18:56:07 -0700

    Update puzzle3.js

--- puzzle3.js
+++ puzzle3.js
@@ -5,7 +5,7 @@ function get_hint() {
 function check_answer() {
   if (document.getElementById("one-one").value == 'R' && document.getElementById("one-two").value == 'E' && document.getElementById("one-three").value == 'D')
   {
-    if (document.getElementById("two-one").value == 'D' && document.getElementById("two-two").value == 'O' && document.getElementById("two-three").value == 'O' && document.getElementById("two-three").value == 'R')
+    if (document.getElementById("two-one").value == 'D' && document.getElementById("two-two").value == 'O' && document.getElementById("two-three").value == 'O' && document.getElementById("two-four").value == 'R')
     {
       if (document.getElementById("three-one").value == 'C' && document.getElementById("three-two").value == 'A' && document.getElementById("three-three").value == 'F' && document.getElementById("three-four").value == 'E')
       {
